1f9fccf00abfc71fcee9fbf615d6b50fac45aa44
tratchapong <tratchapong@gmail.com>
2025-10-31 11:05:20 +0700

    isSubmmitting in RegisterForm

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -5,55 +5,64 @@ import axios from "axios"
 import { useEffect } from "react"
 import { toast } from "react-toastify"
 
-function RegisterForm({resetForm}) {
-	const {handleSubmit, register, formState, reset} = useForm({
-		resolver : zodResolver(registerSchema),
-		mode: 'onBlur'
+function RegisterForm({ resetForm }) {
+	const { handleSubmit, register, formState, reset } = useForm({
+		resolver: zodResolver(registerSchema),
+		mode: 'onSubmit'
 	})
-	const {isSubmitting, errors} = formState
-	useEffect( ()=>{ reset()}, [resetForm])
+	const { isSubmitting, errors } = formState
+	useEffect(() => { reset() }, [resetForm])
 
 	const onSubmit = async data => {
 		try {
-		const resp = await axios.post('http://localhost:8899/api/auth/register', data)
-		toast.success(resp.data?.message)
-		document.getElementById("register-form").close()
-		reset()
-		}catch(err){
+			await new Promise(resolve => setTimeout(resolve, 1000))
+			const resp = await axios.post('http://localhost:8899/api/auth/register', data)
+			toast.success(resp.data?.message)
+			document.getElementById("register-form").close()
+			reset()
+		} catch (err) {
 			console.log(err)
 			const errMsg = err.response?.data?.message || err.message
 			toast.error(errMsg)
 		}
 	}
+	const onError = err => {
+		console.log(err)
+	}
 	return (
 		<>
 			<div className="text-3xl text-center opacity-70">Create a new account</div>
 			<div className="divider opacity-70"></div>
-			<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 p-4 pt-3'>
-				<div className="flex gap-2">
-					<div className="w-full">
-						<input type="text" className="input w-full" placeholder='First name' {...register('firstName')} />
-						<p className="text-sm text-error">{errors.firstName?.message}</p>
+			<form onSubmit={handleSubmit(onSubmit, onError)} >
+				<fieldset disabled={isSubmitting} className='flex flex-col gap-5 p-4 pt-3'>
+					<div className="flex gap-2">
+						<div className="w-full">
+							<input type="text" className="input w-full" placeholder='First name' {...register('firstName')} />
+							<p className="text-sm text-error">{errors.firstName?.message}</p>
+						</div>
+						<div className="w-full">
+							<input type="text" className="input w-full" placeholder='Last name' {...register('lastName')} />
+							<p className="text-sm text-error">{errors.lastName?.message}</p>
+						</div>
 					</div>
 					<div className="w-full">
-						<input type="text" className="input w-full" placeholder='Last name' {...register('lastName')}/>
-						<p className="text-sm text-error">{errors.lastName?.message}</p>
+						<input type="text" className="input w-full" placeholder='Email or Phone number' {...register('identity')} />
+						<p className="text-sm text-error">{errors.identity?.message}</p>
 					</div>
-				</div>
-				<div className="w-full">
-					<input type="text" className="input w-full" placeholder='Email or Phone number' {...register('identity')}/>
-					<p className="text-sm text-error">{errors.identity?.message}</p>
-				</div>
-				<div className="w-full">
-					<input type="password" className="input w-full" placeholder='New Password' {...register('password')} />
-					<p className="text-sm text-error">{errors.password?.message}</p>
-				</div>
-				<div className="w-full">
-					<input type="password" className="input w-full" placeholder='Confirm Password' {...register('confirmPassword')}/>
-					<p className="text-sm text-error">{errors.confirmPassword?.message}</p>
-				</div>
-				<button className="btn btn-secondary text-xl text-neutral-50">Sign up</button>
-				<button className="btn btn-error text-xl text-neutral-50" type="button" onClick={()=>reset()}>Reset Form</button>
+					<div className="w-full">
+						<input type="password" className="input w-full" placeholder='New Password' {...register('password')} />
+						<p className="text-sm text-error">{errors.password?.message}</p>
+					</div>
+					<div className="w-full">
+						<input type="password" className="input w-full" placeholder='Confirm Password' {...register('confirmPassword')} />
+						<p className="text-sm text-error">{errors.confirmPassword?.message}</p>
+					</div>
+					<button className="btn btn-secondary text-xl text-neutral-50">
+						Sign up
+						{isSubmitting && <span className="loading loading-dots loading-sm"></span>}
+					</button>
+					<button className="btn btn-error text-xl text-neutral-50" type="button" onClick={() => reset()}>Reset Form</button>
+				</fieldset>
 			</form>
 		</>
 
