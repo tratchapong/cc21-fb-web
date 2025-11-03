aa376850de4a05c6cfb63a88e8c98ec510f956db
tratchapong <tratchapong@gmail.com>
2025-11-03 16:15:54 +0700

    createPost in PostForm done

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,18 +3,47 @@ import { PhotoIcon2 } from "../icons"
 import useUserStore from "../stores/userStore"
 import Avatar from "./Avatar"
 import AddPicture from "./AddPicture"
+import axios from "axios"
+import { toast } from "react-toastify"
+
 
 function PostForm() {
 	const user = useUserStore(state => state.user)
+	const token = useUserStore(state => state.token)
 	const [addPic, setAddPic] = useState(false)
 	const [file, setFile] = useState(null)
+	const [message, setMessage] = useState('')
 
+	const hdlCreatePost = async () => {
+		let imageUrl = ''
+		try {
+			// upload file ไปที่ cloudinary => ได้ secure_url
+			if (file) {
+				const formData = new FormData()
+				formData.append('file', file)
+				formData.append('upload_preset', 'cc21-upload')
+				const resp = await axios.post('https://api.cloudinary.com/v1_1/tratchapong/image/upload', formData)
+				imageUrl = resp.data.secure_url
+			}
+			// เอา secure_url ที่ได้รวมเป็น body ส่งให้ backend /api/post {message, image}
+			const body = {message : message, image : imageUrl }
+			const resp = await axios.post('http://localhost:8899/api/post', body, {
+				headers : { Authorization : `Bearer ${token}`}
+			})
+			toast.success(resp.data.message)
+			document.getElementById('postform-modal').close()
+		} catch (err) {
+			console.log(err)
+			const errMsg =  err.response?.data.error || err.message
+			toast.error(errMsg)
+		}
+	}
 	return (
 		<div className="flex flex-col gap-2">
 			<h3 className="text-xl text-center">Create post</h3>
 			<div className="divider mt-1 mb-0"></div>
 			<div className="flex gap-2">
-				<Avatar className='w-11 h-11 rounded-full' imgSrc={user.profileImage}/>
+				<Avatar className='w-11 h-11 rounded-full' imgSrc={user.profileImage} />
 				<div className="flex flex-col">
 					<div className="text-sm">{user.firstName} {user.lastName}</div>
 					<select className="select bg-slate-200 select-xs w-full max-w-xs">
@@ -24,17 +53,20 @@ function PostForm() {
 					</select>
 				</div>
 			</div>
-			<textarea className="textarea textarea-ghost w-full" 
-				placeholder={`what do you think? ${user.firstName}` }></textarea>
-			{ addPic && <AddPicture file={file} setFile={setFile}/> }
+			<textarea className="textarea textarea-ghost w-full"
+				placeholder={`what do you think? ${user.firstName}`}
+				onChange={e => setMessage(e.target.value)}
+				value={message}
+			></textarea>
+			{addPic && <AddPicture file={file} setFile={setFile} />}
 			<div className="flex border rounded-lg p-2 justify-between items-center">
 				<p>add with your post</p>
 				<div className="flex justify-center items-center w-10 h-10 rounded-full bg-slate-100
-				 hover:bg-slate-200 active:scale-110" onClick={()=>setAddPic(prv=>!prv)}>
+				 hover:bg-slate-200 active:scale-110" onClick={() => setAddPic(prv => !prv)}>
 					<PhotoIcon2 className='w-7' />
-				 </div>
+				</div>
 			</div>
-			<button className="btn btn-sm btn-primary">Create Post</button>
+			<button className="btn btn-sm btn-primary" onClick={hdlCreatePost}>Create Post</button>
 		</div>
 	)
 }
