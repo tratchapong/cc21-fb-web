8efebdd5ca102e61f975a6dd9719aa643d552f96
tratchapong <tratchapong@gmail.com>
2025-11-04 11:20:42 +0700

    interceptor of postApi done

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,14 +1,23 @@
 import axios from "axios"
+import useUserStore from "../stores/userStore"
 
 export const postApi = axios.create({
 	baseURL : 'http://localhost:8899/api/post'
 })
 
-const addToken = token => ({
-	headers: { Authorization : `Bearer ${token}`}
+postApi.interceptors.request.use( config => {
+	const token = useUserStore.getState().token
+	if(token) {
+		config.headers.Authorization = `Bearer ${token}`
+	}
+	return config
 })
 
-export const createPost = (body, token) => postApi.post('/', body, addToken(token))
+// const addToken = token => ({
+// 	headers: { Authorization : `Bearer ${token}`}
+// })
 
-export const getAllPosts = (token) => postApi.get('/', addToken(token))
+export const createPost = (body) => postApi.post('/', body)
+
+export const getAllPosts = () => postApi.get('/')
 
--- src/stores/postStore.js
+++ src/stores/postStore.js
@@ -1,19 +1,18 @@
 import { create } from "zustand";
 import { createPost, getAllPosts } from "../api/postApi";
-import useUserStore from "./userStore";
 
 const usePostStore = create( (set,get)=> ({
 	posts : [],
 	currentPost : null,
 	createPost : async (body) => {
-		const resp = await createPost(body, useUserStore.getState().token)
+		const resp = await createPost(body)
 		console.log(resp.data)
 		// set({posts : resp.data.result})
 		get().getAllPosts()
 		return resp
 	},
 	getAllPosts : async () => {
-		const resp = await getAllPosts(useUserStore.getState().token)
+		const resp = await getAllPosts()
 		set({posts : resp.data.posts})
 		return resp
 	}
